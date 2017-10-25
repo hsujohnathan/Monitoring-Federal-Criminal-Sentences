62ffb632bee0a57133f815ca8b8d69e95c0c1093
hsujohnathan <minghsu@uw.edu>
2017-10-24 17:43:56 -0700

    initial commit

---
+++ scripts/organize.R
@@ -0,0 +1,27 @@
+# installing package haven here to read sav. file
+library(haven)
+
+# Read existing csv files in to clean. 
+data_95_to_96 <- read.csv("clean_data/95-96.csv")
+data_96_to_97 <- read.csv("clean_data/96-97.csv")
+data_97_to_98 <- read.csv("clean_data/97-98.csv")
+data_1999 <- read.csv("clean_data/1999.csv")
+data_2000 <- read.csv("clean_data/2000.csv")
+data_2001 <- read.csv("clean_data/2001.csv")
+data_2002 <- read.csv("clean_data/2002.csv")
+data_2003 <- read_sav("clean_data/2003.sav")
+data_2004 <- read_sav("clean_data/2004.sav")
+data_2005 <- read_sav("clean_data/2005.sav")
+data_2006 <- read.csv("clean_data/2006.csv") 
+data_2007 <- read.csv("clean_data/2007.csv") 
+data_2008 <- read.csv("clean_data/2008.csv") 
+data_2009 <- read.csv("clean_data/2009.csv") 
+data_2010 <- read.csv("clean_data/2010.csv") 
+data_2011 <- read.csv("clean_data/2011.csv") 
+data_2012 <- read.csv("clean_data/2012.csv") 
+data_2013 <- read.csv("clean_data/2013.csv") 
+data_2014 <- read.csv("clean_data/2014.csv") 
+data_2015 <- read.csv("clean_data/2015.csv") 
+
+
+  
