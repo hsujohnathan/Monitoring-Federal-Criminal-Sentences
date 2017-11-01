5701da97e270b193cddeeecb80a2f66bc0605188
hsujohnathan <minghsu@uw.edu>
2017-11-01 12:43:05 -0700

    Lab 9 commit

--- scripts/merge_data.r
+++ scripts/merge_data.r
@@ -152,28 +152,179 @@ data_2015 <- read.csv("clean_data/selected_data/2015.csv")
   data_2015$EDUCATN <- as.factor(data_2015$EDUCATN)
   data_2015$DISPOSIT <- as.factor(data_2015$DISPOSIT) 
 
+# Cleaning up race variable - this section does a lot of releveling and changes missing/indeterminable to NA. 
+
+data_95_96$MONRACE[data_95_96$MONRACE == "Missing or Indeterminable"] <- NA
+data_95_96$MONRACE <- factor(data_95_96$MONRACE, levels(data_95_96$MONRACE)[c(6,3,1,2,5)])
+levels(data_95_96$MONRACE)
+table(data_95_96$MONRACE)
+data_95_96$MONRACE <- as.numeric(data_95_96$MONRACE)
+
+data_96_97$MONRACE[data_96_97$MONRACE == "Missing"] <- NA
+data_96_97$MONRACE <- factor(data_96_97$MONRACE, levels(data_96_97$MONRACE)[c(6,3,1,2,5)])
+levels(data_96_,97$MONRACE)
+data_96_97$MONRACE <- as.numeric(data_96_97$MONRACE)
+
+data_97_98$MONRACE[data_97_98$MONRACE == "Missing"] <- NA
+data_97_98$MONRACE <- factor(data_97_98$MONRACE, levels(data_97_98$MONRACE)[c(6,3,1,2,5)])
+levels(data_97_98$MONRACE)
+data_97_98$MONRACE <- as.numeric(data_97_98$MONRACE)
+
+data_1999$MONRACE[data_1999$MONRACE == "Multi-racial"] <- NA
+data_1999$MONRACE <- factor(data_1999$MONRACE, levels(data_1999$MONRACE)[c(6,3,1,2,5)])
+levels(data_1999$MONRACE)
+data_1999$MONRACE <- as.numeric(data_1999$MONRACE)
+
+data_2000$MONRACE[data_2000$MONRACE == "Multi-racial"] <- NA
+data_2000$MONRACE <- factor(data_2000$MONRACE, levels(data_2000$MONRACE)[c(6,3,1,2,5)])
+levels(data_2000$MONRACE)
+data_2000$MONRACE <- as.numeric(data_2000$MONRACE)
+
+data_2001$MONRACE[data_2001$MONRACE == "Multi-racial"] <- NA
+data_2001$MONRACE <- factor(data_2001$MONRACE, levels(data_2001$MONRACE)[c(6,3,1,2,5)])
+levels(data_2001$MONRACE)
+data_2001$MONRACE <- as.numeric(data_2001$MONRACE)
+
+data_2002$MONRACE[data_2002$MONRACE == "Multi-racial"] <- NA
+data_2002$MONRACE <- factor(data_2002$MONRACE, levels(data_2002$MONRACE)[c(6,3,1,2,5)])
+levels(data_2002$MONRACE)
+data_2002$MONRACE <- as.numeric(data_2002$MONRACE)
+
+data_2003$MONRACE[data_2003$MONRACE == 7] <- 5
+data_2003$MONRACE <- factor(data_2003$MONRACE, levels(data_2003$MONRACE)[c(1:5)])
+levels(data_2003$MONRACE)
+table(data_2003$MONRACE)
+data_2003$MONRACE <- as.numeric(data_2003$MONRACE)
+
+table(data_2004$MONRACE)
+data_2004$MONRACE[data_2004$MONRACE == 7] <- 5
+data_2004$MONRACE <- factor(data_2004$MONRACE, levels(data_2004$MONRACE)[c(1:5)])
+levels(data_2004$MONRACE)
+data_2004$MONRACE <- as.numeric(data_2004$MONRACE)
+
+table(data_2005$MONRACE)
+data_2005$MONRACE[data_2005$MONRACE == 7] <- 5
+data_2005$MONRACE <- factor(data_2005$MONRACE, levels(data_2005$MONRACE)[c(1:5)])
+levels(data_2005$MONRACE)
+data_2005$MONRACE <- as.numeric(data_2005$MONRACE)
+
+table(data_2006$MONRACE)
+data_2006$MONRACE[data_2006$MONRACE == 7] <- 5
+data_2006$MONRACE <- factor(data_2006$MONRACE, levels(data_2006$MONRACE)[c(1:5)])
+levels(data_2006$MONRACE)
+data_2006$MONRACE <- as.numeric(data_2006$MONRACE)
+
+table(data_2007$MONRACE)
+data_2007$MONRACE[data_2007$MONRACE == 6] <- 5
+data_2007$MONRACE[data_2007$MONRACE == 7] <- 5
+data_2007$MONRACE[data_2007$MONRACE == 8] <- NA
+data_2007$MONRACE <- factor(data_2007$MONRACE, levels(data_2007$MONRACE)[c(1:5)])
+levels(data_2007$MONRACE)
+data_2007$MONRACE <- as.numeric(data_2007$MONRACE)
+
+
+table(data_2008$MONRACE)
+data_2008$MONRACE[data_2008$MONRACE == 6] <- 5
+data_2008$MONRACE[data_2008$MONRACE == 7] <- 5
+data_2008$MONRACE[data_2008$MONRACE == 8] <- NA
+data_2008$MONRACE <- factor(data_2008$MONRACE, levels(data_2008$MONRACE)[c(1:5)])
+levels(data_2008$MONRACE)
+data_2008$MONRACE <- as.numeric(data_2008$MONRACE)
+
+table(data_2009$MONRACE)
+data_2009$MONRACE[data_2009$MONRACE == 7] <- 5
+data_2009$MONRACE[data_2009$MONRACE == 8] <- NA
+data_2009$MONRACE[data_2009$MONRACE == 9] <- 5
+data_2009$MONRACE <- factor(data_2009$MONRACE, levels(data_2009$MONRACE)[c(1:5)])
+levels(data_2009$MONRACE)
+data_2009$MONRACE <- as.numeric(data_2009$MONRACE)
+
+table(data_2010$MONRACE)
+data_2010$MONRACE[data_2010$MONRACE == 7] <- 5
+data_2010$MONRACE[data_2010$MONRACE == 8] <- NA
+data_2010$MONRACE[data_2010$MONRACE == 9] <- 5
+data_2010$MONRACE <- factor(data_2010$MONRACE, levels(data_2010$MONRACE)[c(1:5)])
+levels(data_2010$MONRACE)
+data_2010$MONRACE <- as.numeric(data_2010$MONRACE)
+
+table(data_2011$MONRACE)
+data_2011$MONRACE[data_2011$MONRACE == 7] <- 5
+data_2011$MONRACE[data_2011$MONRACE == 8] <- NA
+data_2011$MONRACE[data_2011$MONRACE == 9] <- 5
+data_2011$MONRACE <- factor(data_2011$MONRACE, levels(data_2011$MONRACE)[c(1:5)])
+levels(data_2011$MONRACE)
+data_2011$MONRACE <- as.numeric(data_2011$MONRACE)
+
+table(data_2012$MONRACE)
+data_2012$MONRACE[data_2012$MONRACE == 7] <- 5
+data_2012$MONRACE[data_2012$MONRACE == 8] <- NA
+data_2012$MONRACE[data_2012$MONRACE == 9] <- 5
+data_2012$MONRACE <- factor(data_2012$MONRACE, levels(data_2012$MONRACE)[c(1:5)])
+levels(data_2012$MONRACE)
+data_2012$MONRACE <- as.numeric(data_2012$MONRACE)
+
+table(data_2013$MONRACE)
+data_2013$MONRACE[data_2013$MONRACE == 7] <- 5
+data_2013$MONRACE[data_2013$MONRACE == 8] <- NA
+data_2013$MONRACE[data_2013$MONRACE == 9] <- 5
+data_2013$MONRACE <- factor(data_2013$MONRACE, levels(data_2013$MONRACE)[c(1:5)])
+levels(data_2013$MONRACE)
+data_2013$MONRACE <- as.numeric(data_2013$MONRACE)
+
+table(data_2014$MONRACE)
+data_2014$MONRACE[data_2014$MONRACE == 7] <- 5
+data_2014$MONRACE[data_2014$MONRACE == 8] <- NA
+data_2014$MONRACE[data_2014$MONRACE == 9] <- 5
+data_2014$MONRACE <- factor(data_2014$MONRACE, levels(data_2014$MONRACE)[c(1:5)])
+levels(data_2014$MONRACE)
+data_2014$MONRACE <- as.numeric(data_2014$MONRACE)
+
+table(data_2015$MONRACE)
+data_2015$MONRACE[data_2015$MONRACE == 7] <- 5
+data_2015$MONRACE[data_2015$MONRACE == 8] <- NA
+data_2015$MONRACE[data_2015$MONRACE == 9] <- 5
+data_2015$MONRACE[data_2015$MONRACE == 10] <- NA
+data_2015$MONRACE <- factor(data_2015$MONRACE, levels(data_2015$MONRACE)[c(1:5)])
+levels(data_2015$MONRACE)
+data_2015$MONRACE <- as.numeric(data_2015$MONRACE)
+
+# cleans up MONSEX variable
+levels(data_95_96$MONSEX)
+
+levels(data_96_97$MONSEX)
+
+levels(data_97_98$MONSEX)
+
+levels(data_1999$MONSEX)
+table(data_1999$MONSEX)
+
+
 # merge data 
 
 data_bind <- rbind(data_95_96,
-                          data_96_97,
-                          data_97_98,
-                          data_1999,
-                          data_2000,
-                          data_2001,
-                          data_2002,
-                          data_2003,
-                          data_2004,
-                          data_2005,
-                          data_2006,
-                          data_2007,
-                          data_2008,
-                          data_2009,
-                          data_2010,
-                          data_2011,
-                          data_2012,
-                          data_2013,
-                          data_2014,
-                          data_2015)
+                   data_96_97,
+                   data_97_98,
+                   data_1999,
+                   data_2000,
+                   data_2001,
+                   data_2002,
+                   data_2003,
+                   data_2004,
+                   data_2005,
+                   data_2006,
+                   data_2007,
+                   data_2008,
+                   data_2009,
+                   data_2010,
+                   data_2011,
+                   data_2012,
+                   data_2013,
+                   data_2014,
+                   data_2015)
+
+unique(data_bind$MONRACE)
+data_bind$MONRACE <- as.numeric(data_bind$MONRACE)
+plot(table(data_bind$MONRACE))
 
 # summary to check NAs
 summary(data_bind)
@@ -186,7 +337,8 @@ ggplot(data_bind, aes(x = XFOLSOR, y = TOTPRISN)) + geom_smooth(method = "loess"
 # model that you run
 # data_bind$TOTPRISN <- as.numeric(paste(data_bind$TOTPRISN))
 race.lm <- lm(TOTPRISN ~ + AGE + DISPOSIT + XFOLSOR * XCRHISSR + DISTRICT +
-              MONCIRC + MONSEX + YEAR + MONRACE, data_bind)
+              MONCIRC + MONSEX + YEAR + as.factor(MONRACE), data_bind)
+summary(race.lm)
 # export data to the variable data folder
   
 write.csv(data_95_96, "clean_data/variable_data/95-96.csv")
